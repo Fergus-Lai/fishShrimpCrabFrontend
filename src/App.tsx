0286d0735c05f86bdbee91ef8d74a2496f8b0569
Fergus Lai <ferguslai724@gmail.com>
2022-11-05 15:43:24 +0000

    Added anya and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,56 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p className="text-3xl font-bold underline">Hello World</p>
+    <div className="bg-cyan-900">
+      <div className="flex place-content-center">
+        <p>dice</p>
+      </div>
+      <div className="flex flex-row flex-wrap">
+        <div className="basis-full md:basis-1/3">
+          <img
+            className="mx-auto"
+            src={require("./imgs/anya.png")}
+            alt="anya"
+          />
+        </div>
+        <div className="basis-full md:basis-1/3">
+          <img
+            className="mx-auto"
+            src={require("./imgs/anya.png")}
+            alt="anya"
+          />
+        </div>
+        <div className="basis-full md:basis-1/3">
+          <img
+            className="mx-auto"
+            src={require("./imgs/anya.png")}
+            alt="anya"
+          />
+        </div>
+        <div className="basis-full md:basis-1/3">
+          <img
+            className="mx-auto"
+            src={require("./imgs/anya.png")}
+            alt="anya"
+          />
+        </div>
+        <div className="basis-full md:basis-1/3">
+          <img
+            className="mx-auto"
+            src={require("./imgs/anya.png")}
+            alt="anya"
+          />
+        </div>
+        <div className="basis-full md:basis-1/3">
+          <img
+            className="mx-auto"
+            src={require("./imgs/anya.png")}
+            alt="anya"
+          />
+        </div>
+      </div>
     </div>
   );
 }
